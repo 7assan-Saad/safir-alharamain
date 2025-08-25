3b381e140fe26da5ff8fbb4a3dbd3c734691a5b7
Hassan Saad <hassan.saad.dev@gmail.com>
2025-08-25 17:38:07 +0300

    Added new insid trips

--- js/home.js
+++ js/home.js
@@ -80,57 +80,57 @@ const latestUmrahTrips = (async () => {
 })()
 
 // latest travel offers
-// const latestTravelPro = document.querySelector('.travel-latest')
-// const latestTravelTrips = (async () => {
-//   let trips = []
-//   let tripsSlice
-//   await db.collection('trips').where('category', '==', 'insideTrip')
-//   .where('status', '==', 'new').get()
-//   .then(category => {
-//     category.forEach(cat => {
-//       let obj = cat.data()
-//       obj.id = `${cat.id}`
-//       trips.push(obj)
-//     })
-//     // Sort the trips array by 'quadruplePrice' from lowest to highest
-//     trips.sort((a, b) => a.sort - b.sort)
-//     tripsSlice = trips.slice(0, 4)
-//     // console.log(tripsSlice)
-//   })
+const latestTravelPro = document.querySelector('.travel-latest')
+const latestTravelTrips = (async () => {
+  let trips = []
+  let tripsSlice
+  await db.collection('trips').where('category', '==', 'insideTrip')
+  .where('status', '==', 'new').get()
+  .then(category => {
+    category.forEach(cat => {
+      let obj = cat.data()
+      obj.id = `${cat.id}`
+      trips.push(obj)
+    })
+    // Sort the trips array by 'quadruplePrice' from lowest to highest
+    trips.sort((a, b) => a.sort - b.sort)
+    tripsSlice = trips.slice(0, 4)
+    // console.log(tripsSlice)
+  })
 
-//   let renderProducts = tripsSlice.map(product => { 
-//     return /*html*/`
-//       <div class="col p-3">
-//         <div
-//           class="product d-flex justify-content-center position-relative overflow-hidden mx-auto rounded-5 text-center text-white cursor-pointer"
-//           onclick="getID('${product.id}')">
-//           <img src="./img/inside-trip-imgs/${product.imgs[0]}" class="h-100" alt="img">
-//           <!-- <div class="product-company">
-//             <img src="./img/${product.company.companyLogo}" alt="logo">
-//           </div> -->
-//           ${
-//             product.tripDay != "" ? /*html*/`
-//               <div class="trip-date">
-//                 <span class="day fs-7 d-block">${product.tripDay}</span>
-//                 <span class="day-num d-block">${product.tripDayNum}</span>
-//                 <span class="month fs-7 d-block">${product.tripMonth}</span>
-//               </div>
-//             ` : ''
-//           }
-//           <div class="product-text">
-//             <h3 class="fs-4 mb-1">${product.tripTitle}</h3>
-//             ${
-//               product.mainPrice != "" ? /*html*/`
-//                 <div class="d-flex align-items-center justify-content-center">
-//                   <p class="fs-3 text-bold mb-0">${product.mainPrice}</p>
-//                   <!-- <span class="fs-6 text-regular d-block" style="margin-top: -5px; margin-right: 5px;">جنيه مصري</span> -->
-//                 </div>
-//               ` : ''
-//             }
-//           </div>
-//         </div>
-//       </div>
-//     `
-//   })
-//   latestTravelPro.innerHTML = renderProducts.join('')
-// })()
+  let renderProducts = tripsSlice.map(product => { 
+    return /*html*/`
+      <div class="col p-3">
+        <div
+          class="product d-flex justify-content-center position-relative overflow-hidden mx-auto rounded-5 text-center text-white cursor-pointer"
+          onclick="getID('${product.id}')">
+          <img src="./img/inside-trip-imgs/${product.imgs[0]}" class="h-100" alt="img">
+          <!-- <div class="product-company">
+            <img src="./img/${product.company.companyLogo}" alt="logo">
+          </div> -->
+          ${
+            product.tripDay != "" ? /*html*/`
+              <div class="trip-date">
+                <span class="day fs-7 d-block">${product.tripDay}</span>
+                <span class="day-num d-block">${product.tripDayNum}</span>
+                <span class="month fs-7 d-block">${product.tripMonth}</span>
+              </div>
+            ` : ''
+          }
+          <div class="product-text">
+            <h3 class="fs-4 mb-1">${product.tripTitle}</h3>
+            ${
+              product.mainPrice != "" ? /*html*/`
+                <div class="d-flex align-items-center justify-content-center">
+                  <p class="fs-3 text-bold mb-0">${product.mainPrice}</p>
+                  <!-- <span class="fs-6 text-regular d-block" style="margin-top: -5px; margin-right: 5px;">جنيه مصري</span> -->
+                </div>
+              ` : ''
+            }
+          </div>
+        </div>
+      </div>
+    `
+  })
+  latestTravelPro.innerHTML = renderProducts.join('')
+})()
